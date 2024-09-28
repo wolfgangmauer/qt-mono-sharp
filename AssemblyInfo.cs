f8d941ad99a51d307e9c1b5b49b862b775b40335
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-09-28 13:41:21 +0200

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Runtime.CompilerServices;
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyFile("qt-mono-sharp.snk")]
