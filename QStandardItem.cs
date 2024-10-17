03a836ff6790adf016fd06b791dde0e4b75140ce
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-17 15:14:58 +0200

    Update QStandardItem.cs

--- QStandardItem.cs
+++ QStandardItem.cs
@@ -10,10 +10,10 @@ namespace Qt
 		public IntPtr Handle { get { return raw; }}
 
 		[MethodImpl (MethodImplOptions.InternalCall)]
-		protected static extern IntPtr qt_standarditem_new (string text);
+		protected static extern IntPtr qt_standarditem_new (StandardItem thisObject, string text);
 		public StandardItem (string text)
 		{
-			raw = qt_standarditem_new (text);
+			raw = qt_standarditem_new (this, text);
 		}
 
 		public StandardItem (IntPtr raw)
@@ -29,8 +29,8 @@ namespace Qt
 		protected static extern void qt_standarditem_text_set (IntPtr handle, string text);
 		public string Text
 		{
-			get { return qt_standarditem_text_get(raw); }
-			set { qt_standarditem_text_set(raw, value); }
+			get { return qt_standarditem_text_get(Handle); }
+			set { qt_standarditem_text_set(Handle, value); }
 		}
 	}
 }
