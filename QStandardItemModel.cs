e7f9dcd55d104434d232e75a2fa4d59f968336d5
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-17 11:30:52 +0200

    Update QStandardItemModel.cs

--- QStandardItemModel.cs
+++ QStandardItemModel.cs
@@ -47,6 +47,12 @@ namespace Qt
 		{
 			qt_standarditemmodel_item_row_col_set (Handle, row, col, item.Handle);
 		}
+  		[MethodImpl (MethodImplOptions.InternalCall)]
+		protected static extern IntPtr qt_standarditemmodel_item_row_col_get (IntPtr handle, int row, int col);
+		public StandardItem GetItem(int row, int col)
+		{
+			return new StandardItem(qt_standarditemmodel_item_row_col_set (Handle, row, col));
+		}
 		[MethodImpl (MethodImplOptions.InternalCall)]
 		protected static extern void qt_standarditemmodel_data_row_col_set (IntPtr handle, int row, int col, string item);
 		public void SetData(int row, int col, string item)
