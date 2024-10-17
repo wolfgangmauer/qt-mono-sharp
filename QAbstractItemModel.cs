8f0f903b86cdffa2a49906c94e166bb12ca9f501
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-17 09:46:05 +0200

    Update QAbstractItemModel.cs

--- QAbstractItemModel.cs
+++ QAbstractItemModel.cs
@@ -5,7 +5,6 @@ namespace Qt
 	public abstract class AbstractItemModel : Object
 	{
 		protected AbstractItemModel (IntPtr raw) : base (raw) { }
-  		protected virtual object data(ModelIndex index, int role = Qt.DisplayRole);
 	}
 }
 
