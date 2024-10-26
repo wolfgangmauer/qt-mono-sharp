e1288679bf0927934c8d6545af1f6e48acabcc77
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-26 14:34:30 +0200

    Update QWidget.cs

--- QWidget.cs
+++ QWidget.cs
@@ -36,7 +36,7 @@ namespace Qt
 
 		protected Widget () { }
 
-		public Widget (IntPtr raw) : base(raw) {	}
+		protected Widget (IntPtr raw) : base(raw) {	}
 
 		protected Widget (Widget parent, string uiFile, WindowType f = 0) : base (IntPtr.Zero)
 		{
