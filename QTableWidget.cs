41c05b0424e078628061861506080b1947bafd18
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2024-10-26 14:43:08 +0200

    Update QTableWidget.cs

--- QTableWidget.cs
+++ QTableWidget.cs
@@ -59,7 +59,7 @@ namespace Qt
     	public TableWidgetItem Item(int row, int col)
     	{
             IntPtr retVal = qt_tablewidget_row_col_get (Handle, row, col);
-    		return retVal != IntPtr.Zero ? new TableWidgetItem(retVal) : null;
+    		return retVal != IntPtr.Zero ? TableWidgetItem.CreateFromRaw(retVal) : null;
     	}
     
     	[MethodImpl (MethodImplOptions.InternalCall)]
